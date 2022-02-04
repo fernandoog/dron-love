d6721b57f7d172c61461763b63b2682bd6f2ca73
Fernando Ortega <fernandoog@gmail.com>
2022-02-04 17:14:10 +0100

    Initial commit

---
+++ hello_drone.py
@@ -0,0 +1,39 @@
+# Import DroneKit-Python
+from dronekit import connect, VehicleMode
+import dronekit_sitl
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print
+    "Start simulator (SITL)"
+
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+    # Connect to the Vehicle.
+    print("Connecting to vehicle on: %s" % (connection_string,))
+    vehicle = connect(connection_string, wait_ready=True)
+
+    # Get some vehicle attributes (state)
+    print
+    "Get some vehicle attribute values:"
+    print
+    " GPS: %s" % vehicle.gps_0
+    print
+    " Battery: %s" % vehicle.battery
+    print
+    " Last Heartbeat: %s" % vehicle.last_heartbeat
+    print
+    " Is Armable?: %s" % vehicle.is_armable
+    print
+    " System status: %s" % vehicle.system_status.state
+    print
+    " Mode: %s" % vehicle.mode.name  # settable
+
+    # Close vehicle object before exiting script
+    vehicle.close()
+
+    # Shut down simulator
+    sitl.stop()
+    print("Completed")
